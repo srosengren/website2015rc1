cf098740baf46454221af5e35830cba3ea29b4f7
Sebastian Rosengren <sebastian@rosengren.me>
2015-06-26 22:56:09 +0200

    initial

---
+++ src/Website2015RC1/Controllers/Api/WebApiBaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Website2015RC1.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public abstract class WebApiBaseController : Controller
+    {
+    }
+}
